009cf795d113cfe47c2579a6c3c86e455365fce9
leapfrog-saugat <saugatgautam@lftechnology.com>
2015-06-04 11:22:51 +0545

    Initial Commit

---
+++ MSDynamicsDrawerViewController_Sample1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MSDynamicsDrawerViewController_Sample1
+//
+//  Created by Saugat Gautam on 6/4/15.
+//  Copyright (c) 2015 Saugat Gautam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
